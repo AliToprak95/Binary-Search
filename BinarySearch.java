33241e7672e94cb0e6a0eade9ec1ba3ae891c384
Ali Toprak <45083841+AliToprak95@users.noreply.github.com>
2018-11-25 00:03:29 +0000

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,42 @@
+package pack;
+
+//ALI TOPRAK 201854644 MA407
+
+public class BinarySearch {
+	
+	static boolean found=false;
+	static int counter=0;
+	
+	static int binsearch(int key, int[] x, int i, int j) {
+		counter++;
+		if(i==j) {
+			return i;			
+			}
+		else if(key<=x[(i+j)/2]) {
+			return binsearch(key, x, i , (i+j)/2);
+		}
+		else {
+			return binsearch(key, x, ((i+j)/2)+1, j);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		int key=11;
+		int[] x=new int[10000000];
+		for(int i=0;i<x.length;i++) {
+			x[i]=10;
+		}
+		counter=0;
+		int index=binsearch(key,x,0,10000000);
+		if(index<x.length && x[index]==key) found=true;
+		if(found==true) {
+			System.out.println("Key "+key+" was found at index "+index+", after "+counter+" binary search iterations");
+		}
+		else {
+			System.out.println("Key "+key+" not found, should be at index "+index+", after "+counter+" binary search iterations");
+		}
+
+	}
+
+}
